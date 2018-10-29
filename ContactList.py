e8b6f4e362238914873fc51ee94ef59b4b2a52f3
Evan Pomponio <32994398+epomp447@users.noreply.github.com>
2018-10-29 16:52:27 -0400

    Add files via upload

--- ContactList.py
+++ ContactList.py
@@ -6,26 +6,30 @@ def init():
 
 # create an empty list
     contacts = []
-    print("Enter size: ")
-    size = int(input())
-
-    for i in range(size):
-        print("Enter Contact ", i, " Name:")
-        name = input()
-        print("Enter Contact ", i, " Phone:")
-        phone = int(input())
-        print("Enter Student ", i, " email:")
-        email = input()
-
-# dynamically add to the list a dictionary (hash) item
-        contacts.append({'name':name, 'phone':phone, 'email':email})
-
+    try:
+        print("Enter size: ")
+        size = int(input())
+        for i in range(size):
+            print("Enter Contact ", i, " Name:")
+            name = input()
+            print("Enter Contact ", i, " Phone:")
+            phone = int(input())
+            print("Enter Student ", i, " email:")
+            email = input()
+            # dynamically add to the list a dictionary (hash) item
+            contacts.append({'name': name, 'phone': phone, 'email': email})
+    except TypeError:
+        print("Invalid Input: ")
+        exit(1)
+    except ValueError:
+        print("Invalid Input: ")
+        exit(1)
     return contacts
 #####################################################
 # print the list
 def display(s):
     for i in range(len(s)):
-        print(s[i]['name']," ", s[i]['phone']," ",s[i]['email'])
+        print(s[i]['name'], " ", s[i]['phone'], " ", s[i]['email'])
     return
 #####################################################
 # main module starts here
